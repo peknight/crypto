c39b2737a3489fe17e1292c7382ff6c4a59e9c53
peknight <JKpeknight@gmail.com>
2024-05-16 18:09:39 +0800

    update

--- crypto-core/shared/src/main/scala/com/peknight/crypto/algorithm/cipher/Transformation.scala
+++ crypto-core/shared/src/main/scala/com/peknight/crypto/algorithm/cipher/Transformation.scala
@@ -1,19 +1,26 @@
 package com.peknight.crypto.algorithm.cipher
 
+import com.peknight.crypto.algorithm.Algorithm
+import com.peknight.crypto.algorithm.cipher.asymmetric.RSA
 import com.peknight.crypto.algorithm.cipher.mode.{Mode, NONE}
-import com.peknight.crypto.algorithm.cipher.padding.{NoPadding, Padding}
+import com.peknight.crypto.algorithm.cipher.padding.{NoPadding, OAEP, Padding}
 
-trait Transformation:
+trait Transformation extends Algorithm:
   def cipherForTransformation: Cipher
   def mode: Mode
   def padding: Padding
   def /(mode: Mode): Transformation
   def /(padding: Padding): Transformation
   def transformation: String = s"${cipherForTransformation.abbreviation}/${mode.mode}/${padding.padding}"
+  override def abbreviation: String =
+    (cipherForTransformation, padding) match
+      case (r: RSA, p: OAEP) => "RSA-OAEP"
+      case _ => super.abbreviation
 end Transformation
 object Transformation:
   private[this] case class Transformation(cipherForTransformation: Cipher, mode: Mode, padding: Padding)
     extends com.peknight.crypto.algorithm.cipher.Transformation:
+    def algorithm: String = cipherForTransformation.algorithm
     def /(mode: Mode): Transformation = copy(mode = mode)
     def /(padding: Padding): Transformation = copy(padding = padding)
   end Transformation
